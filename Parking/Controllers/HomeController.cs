99538ce431e4cbfd4eedf746edeb82b0a753696c
LAPTOP-GIJTJIHI\Franek <franek.b93@gmail.com>
2019-04-04 21:21:16 +0200

    Initial commit

---
+++ Parking/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Parking.Data;
+using Parking.Models;
+
+namespace Parking.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            _context.Tests.Add(new Test() { Id = Guid.NewGuid(), Name = "SIEMA"});
+            _context.SaveChanges();
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Parking/Models/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parking.Models
+{
+    public class Test
+    {
+
+        public Guid Id { get; set; }
+        [Column(TypeName = "varchar(200)")]
+        public string Name { get; set; }
+    }
+}
